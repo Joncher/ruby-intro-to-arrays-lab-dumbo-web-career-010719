0fa837fa94f097cddc39ce681a3bad50db09107b
John Crespi Oncher <johnoncher@gmail.com>
2018-12-28 22:08:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,6 +1,6 @@
 @my_new_array = instantiate_new_array
-@my_new_array = []
+instantiate_new_array = []
 
 @my_two_array = array_with_two_elements
-@my_two_array = [0,1]
+array_with_two_elements = [0,1]
 
